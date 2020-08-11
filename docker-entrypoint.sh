1678db829180b8f757991fd781d82fb18c8c35fb
Tarek Ziade <tarek@ziade.org>
2020-08-11 14:30:08 +0200

    bind on host 0.0.0.0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 case "$1" in
     server)
-    exec /home/docker/data/bin/hypercorn --quic-bind localhost:4433 --certfile /home/docker/data/cert.pem --keyfile /home/docker/data/key.pem --bind localhost:8000 mozhttp3server.run:app --access-logfile -
+    exec /home/docker/data/bin/hypercorn --quic-bind 0.0.0.0:4433 --certfile /home/docker/data/cert.pem --keyfile /home/docker/data/key.pem --bind 0.0.0.0:8000 mozhttp3server.run:app --access-logfile -
     ;;
 
     *)
