ce1b3c2bcfae94e8617d885a7d6d49ab3b60106a
Tarek Ziade <tarek@ziade.org>
2020-08-11 13:50:29 +0200

    access-logfile

--- mozhttp3server/run.py
+++ mozhttp3server/run.py
@@ -1,20 +1,6 @@
-import logging
-
 from quart import Quart
-from quart.ctx import _request_ctx_stack
 
-
-
-class LoggedQuart(Quart):
-    async def dispatch_request(self, request_context):
-        request = (request_context or _request_ctx_stack.top).request
-        if self.logger.level != logging.DEBUG:
-            self.logger.setLevel(logging.DEBUG)
-        self.logger.debug(f"{request.method} {request.url}")
-        return await super().dispatch_request(request_context)
-
-
-app = LoggedQuart(__name__)
+app = Quart(__name__)
 
 @app.route("/")
 async def index():
