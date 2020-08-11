8d51285c10b0ee46c6093792c8a49b499ad58dfb
Tarek Ziade <tarek@ziade.org>
2020-08-11 13:36:54 +0200

    custom log

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 case "$1" in
     server)
-    exec /home/docker/data/bin/hypercorn --quic-bind localhost:4433 --certfile cert.pem --keyfile key.pem --bind localhost:8000 mozhttp3server.run:app
+    exec /home/docker/data/bin/hypercorn --quic-bind localhost:4433 --certfile /home/docker/data/cert.pem --keyfile /home/docker/data/key.pem --bind localhost:8000 mozhttp3server.run:app
     ;;
 
     *)
--- mozhttp3server/run.py
+++ mozhttp3server/run.py
@@ -1,6 +1,20 @@
-from quart import Quart
+import logging
 
-app = Quart(__name__)
+from quart import Quart
+from quart.ctx import _request_ctx_stack
+
+
+
+class LoggedQuart(Quart):
+    async def dispatch_request(self, request_context):
+        request = (request_context or _request_ctx_stack.top).request
+        if self.logger.level != logging.DEBUG:
+            self.logger.setLevel(logging.DEBUG)
+        self.logger.debug(f"{request.method} {request.url}")
+        return await super().dispatch_request(request_context)
+
+
+app = LoggedQuart(__name__)
 
 @app.route("/")
 async def index():
