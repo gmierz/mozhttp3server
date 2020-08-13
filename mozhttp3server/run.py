86a18d1d18fc16b345f68da0d1333ac62ef8228d
Tarek Ziade <tarek@ziade.org>
2020-08-13 19:56:53 +0200

    fixed async func

--- mozhttp3server/run.py
+++ mozhttp3server/run.py
@@ -6,22 +6,22 @@ app = Quart(__name__, static_folder="")
 HERE = pathlib.Path(__file__).parent
 
 
-def common_make_push_promise():
-	for static_file in pathlib.Path(HERE, "common").rglob(".*"):
-		await make_push_promise(url_for("static", filename=str(static_file)))
+async def common_make_push_promise():
+    for static_file in pathlib.Path(HERE, "common").rglob(".*"):
+        await make_push_promise(url_for("static", filename=str(static_file)))
 
 
 @app.route("/")
 async def index():
-	return "<b>Hello World!</b>"
+    return "<b>Hello World!</b>"
 
 
 @app.route("/shopping.html")
 async def shopping():
-	common_make_push_promise()
-	for image in pathlib.Path(HERE, "shopping/images").rglob(".*"):
-		await make_push_promise(url_for("", filename=f"shopping/images/{image.name}"))
-	return await send_file(str(pathlib.Path(HERE, "shopping/shopping.html")))
+    await common_make_push_promise()
+    for image in pathlib.Path(HERE, "shopping/images").rglob(".*"):
+        await make_push_promise(url_for("", filename=f"shopping/images/{image.name}"))
+    return await send_file(str(pathlib.Path(HERE, "shopping/shopping.html")))
 
 
 @app.route("/other")
